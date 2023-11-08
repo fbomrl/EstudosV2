5e76a8ff5cbfc0b41c0ae91e38edad1e643c42b0
Fabio Meireles <fbomrl14@gmail.com>
2023-11-08 12:04:18 -0300

    Inclusao de nova atividade em c#

---
+++ C#/EX001_ADA/EX001_ADA/Program.cs
@@ -0,0 +1,97 @@
+﻿
+
+using System.Security.Cryptography.X509Certificates;
+
+class program {
+    static void Main(string[] args)
+    {
+        List<double> saldoTotal = new  List<double>();
+
+        MostrarMenu();
+
+
+
+        void MostrarMenu()
+        {
+            Console.WriteLine("\nMenu de Operação");
+            Console.WriteLine("[1] Mostrar Saldo: ");
+            Console.WriteLine("[2] Efetuar Depósito: ");
+            Console.WriteLine("[3] Efetuar Saque: ");
+            Console.WriteLine("[0] Finalizar: ");
+
+            Console.WriteLine("Digite uma das opções.");
+            string opcao_selecionada = Console.ReadLine();
+            int opcao_escolhida = int.Parse(opcao_selecionada);
+
+            switch(opcao_escolhida)
+            {
+                case 1:
+                    SaldoDisponivel();
+                    Retorno();
+                    break;
+                case 2:
+                    EfetuaDeposito();
+                    Retorno();
+                    break;
+                case 3:
+                    EfetuarSaque();
+                    Retorno();
+                    break;
+                case 0: Console.WriteLine("\nFinalizando...");
+                    break;
+                default: Console.WriteLine("Opção inválida!");
+                    Retorno();
+                    break;
+            }
+        }
+        void SaldoDisponivel()
+        {
+            List<double> saldoTotalDisponivel = saldoTotal;
+            Console.WriteLine($"\nSaldo de R$ {saldoTotalDisponivel.Sum()}");
+        }
+
+        void EfetuaDeposito()
+        {
+            try
+            {
+                Console.WriteLine("\nValor a ser depositado R$ ");
+                string valor = Console.ReadLine();
+                double valorDeposito = Convert.ToDouble(valor);
+                saldoTotal.Add(valorDeposito);
+                Thread.Sleep(1000);
+                if (valorDeposito > 0)
+                {
+                    Console.WriteLine($"\nEfetuando depósito de R${valorDeposito}");
+                }
+                else
+                {
+                    Console.WriteLine("Valor de depósito inválido");
+                    Thread.Sleep(1000);
+                }
+            } catch (FormatException)
+            {
+                Console.WriteLine("\nFavor digitar caracteres numéricos.");
+            }        
+        }
+
+        void EfetuarSaque()
+        {
+            Console.WriteLine("\nQual valor deseja sacar? R$ ");
+            string valor = Console.ReadLine();
+            double valorSaque = Convert.ToDouble(valor);
+            saldoTotal.Add(valorSaque);
+            Thread.Sleep(1000);
+
+            Console.WriteLine($"Realizando saque no valor de R${valorSaque}");
+        }
+
+        void Retorno()
+        {
+            Console.WriteLine("\nDigite qualquer tecla para voltar ao MENU de Operação.");
+            Console.ReadKey();
+            Console.Clear();
+            MostrarMenu();
+        }
+    }
+}
+
