b23514f9c86145d960050304f7134fe1fe4d2b6f
Fabio Meireles <fbomrl14@gmail.com>
2024-03-09 12:19:35 -0300

    Inclusão de atividade C# CRUD

---
+++ C#/SistemasDeTarefas/SistemasDeTarefas/Data/SistemaDeTarefasDBContex.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SistemasDeTarefas.Models;
+
+namespace SistemasDeTarefas.Data
+{
+    public class SistemaDeTarefasDBContex : DbContext
+    {
+        public SistemaDeTarefasDBContex(DbContextOptions<SistemaDeTarefasDBContex> options) 
+            : base (options)
+        {
+
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+
+        public DbSet<TarefaModel> Tarefas {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ C#/SistemasDeTarefas/SistemasDeTarefas/Repositories/UsuarioRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemasDeTarefas.Data;
+using SistemasDeTarefas.Models;
+using SistemasDeTarefas.Repositories.Interfaces;
+
+namespace SistemasDeTarefas.Repositories
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly SistemaDeTarefasDBContex _dbcontext;
+
+        public UsuarioRepositorio(SistemaDeTarefasDBContex sistemaDeTarefasDBContex)
+        {
+            _dbcontext = sistemaDeTarefasDBContex;
+        }
+
+        public async Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            _dbcontext.Usuarios.Add(usuario);
+            _dbcontext.SaveChanges();
+        }
+
+        public Task<bool> Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            return ok();
+        }
+
+        public async Task<UsuarioModel> BuscarPorId(int id)
+        {
+            return await _dbcontext.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return await _dbcontext.Usuarios.ToListAsync();
+        }
+    }
+}
