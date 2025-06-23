49f9e4876ede65ee66c7e9343841d05551e3ba74
Fabio Meireles da Silva <87281049+fbomrl@users.noreply.github.com>
2025-06-23 13:54:20 -0300

    Atividades/Aulas feitas da semana passada pt 1.
    Atividades do curso Curso mais didático e completo de C# e OO: composição, herança, coleções, arquivos, LINQ, lambda, delegates e muito mais
    que foram realizadas ano de 2024 .

---
+++ C#/UDEMY/_109_Upcasting_e_Downcasting/109_Upcasting_e_Downcasting/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,14 @@
+﻿using _109_110_111_112_Aulas_;
+
+
+namespace _109_Upcasting_e_Downcasting.Entities
+{
+    class SavingsAccountPlus : SavingsAccount
+    {
+        //Fica inviável utilizar devido ao sealed
+        //public override void Withdraw(double amount) 
+        //{
+            
+        //} 
+    }
+}
---
+++ C#/UDEMY/_115_Exercicio_proposto/115_Exercicio_proposto/Entities/UsedProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _115_Exercicio_proposto.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+        public UsedProduct(string name, double price, DateTime manufactureDate)
+            : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+        public override string PriceTag()
+        {
+            return $"(Manufacture date {ManufactureDate.ToString("dd/MM/yyyy")})";
+        }
+    }
+}
