16508bf37cef425fd1d8984d2dc49eaa47d869af
Fabio Meireles da Silva <87281049+fbomrl@users.noreply.github.com>
2025-06-23 13:43:03 -0300

    Inclusão de atividades antigas
    Atividades do curso "Curso mais didático e completo de C# e OO: composição, herança, coleções, arquivos, LINQ, lambda, delegates e muito mais"
    que foram realizadas ano de 2024 .

---
+++ C#/UDEMY/_072_ModificadorParametroParams/_072_ModificadorParametroParams/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _072_ModificadorParametroParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Explicação sobre PARAMS...
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 3, 4);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine();
+
+            //Explicação sobre modificador de parametro REF, faz o parâmetro ser uma referência para a variável original
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+
+            //Explicação sobre modificador OUT faz o parâmetro ser uma referência para a variável original, mas não exige que ela seja inicializada;
+            int b = 10;
+            int triple;
+            Calculator.Triple2(b, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+
+}
