743601e2f8055a7e555676bfe4a1b6c880adca95
Fabio Meireles da Silva <87281049+fbomrl@users.noreply.github.com>
2025-06-24 13:56:07 -0300

    Exercícios iniciante Beecrowd

---
+++ C#/Beecrowd/Iniciante/1019_coversao_de_tempo_beecrowd/1019_coversao_de_tempo_beecrowd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+
+        int h = n / 3600;
+        int h2 = n % 3600;
+        int m = h2 / 60;
+        int m2 = h2 % 60;
+        int s = m2;
+
+        Console.WriteLine($"{h}:{m}:{s}");
+
+        Console.ReadLine();
+    }
+}
---
+++ C#/Beecrowd/Iniciante/1021_Notas_e_Moedas_beecrowd/1021_Notas_e_Moedas_beecrowd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double valor = double.Parse(Console.ReadLine());
+
+        int[] notas = { 100, 50, 20, 10, 5, 2 };
+        double[] moedas = { 1.00, 0.50, 0.25, 0.10, 0.05, 0.01 };
+
+        Console.WriteLine("NOTAS:");
+        foreach (var nota in notas)
+        {
+            int qntdNota = (int)(valor / nota);
+            Console.WriteLine($"{qntdNota} nota(s) de R$ {nota:0.00}");
+            valor %= nota;
+        }
+        Console.WriteLine("MOEDAS:");
+        foreach (var moeda in moedas)
+        {
+            double qntdMoeda = (valor / moeda);
+            Console.WriteLine($"{Math.Truncate(qntdMoeda)} moeda(s) de R$ {moeda:0.00}");
+            valor = Math.Round(valor % moeda, 2);
+        }
+
+        Console.ReadLine();
+    }
+}
