e00bc49c639ad5c312231a116600bf3d98d91ef8
Fabio Meireles da Silva <87281049+fbomrl@users.noreply.github.com>
2025-06-24 13:16:53 -0300

    Atividades/Aula Interface C#

---
+++ C#/UDEMY/_138_139_140_Solucao_Sem_Interface/_138_Interface/Entities/Vehicle.cs
@@ -0,0 +1,11 @@
+﻿namespace _138_Interface.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
